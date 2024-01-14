e83b068853f0d71501db9e36cbb4e9758b6ba8cf
Daniel Pastoor <daniel.pastoor@identty.nl>
2024-01-14 13:58:52 +0100

    Added Dashboard

---
+++ src/controllers/DashboardController.py
@@ -0,0 +1,41 @@
+""" Index Controller
+"""
+import flask_login
+# needed imports
+from flask import render_template
+from flask_login import login_required
+
+# own imports
+from src.controllers.Base.ControllerBase import ControllerBase
+from src.data.ApplicationContext import ApplicationContext
+from src.models.BaseModel.TransientObject import TransientObject
+from src.models.BookingEntity import BookingEntity
+
+
+class DashboardController(ControllerBase):
+    """index controller for showing the home page
+
+    Returns:
+        _type_: page
+    """
+
+    @login_required
+    # get
+    def index(self):
+        """ Endpoint for getting the index page
+        """
+
+        print(flask_login.current_user)
+
+        applicationContext = ApplicationContext()
+
+        bookings = applicationContext.Get(BookingEntity(), "*", f"id = {flask_login.current_user.id}")
+
+        # return rendered html
+        return render_template("pages/dashboard/dashboard.html", bookings=bookings)
+
+
+
+
+if __name__ == "__main__":
+    pass
